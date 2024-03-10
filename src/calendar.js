d665a539b60c70d9d83058ebb83270951b27ca12
olesya-amor <74275916+olesya-amor@users.noreply.github.com>
2024-03-10 15:40:26 -0700

    Bunch of updates on calendar component

--- src/calendar.js
+++ src/calendar.js
@@ -1,21 +1,63 @@
-import React, { useState } from 'react'; 
+import React, { useState, useEffect } from 'react'; 
+import Select from 'react-select';
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import interactionPlugin from "@fullcalendar/interaction";
+import eventsData from './events-data.json';
+
+
+const customStyles = {
+    control: (provided, state) => ({
+        ...provided,
+        backgroundColor: '#B6433E', 
+        color: 'white', 
+        border: '1px solid black', 
+    }),
+    option: (provided, state) => ({
+        ...provided,
+        backgroundColor: state.isSelected ? '#CFB034' : '#B6433E', 
+        color: state.isSelected ? 'black' : 'white', 
+        '&:hover': {
+            backgroundColor: '#CFB034',
+            color: 'black'
+        },
+    }),
+    placeholder: (provided, state) => ({
+        ...provided,
+        color: 'white', 
+    }),
+};
 
 function Calendar() {
 
     const [selectedGenre, setSelectedGenre] = useState('all');
     const [selectedSize, setSelectedSize] = useState('all');
     const [selectedCost, setSelectedCost] = useState('all');
-    
+    const [events, setEvents] = useState([]);
+    const [genrePlaceholder, setGenrePlaceholder] = useState('Select Genre');
+    const [sizePlaceholder, setSizePlaceholder] = useState('Select Audience Size');
+    const [costPlaceholder, setCostPlaceholder] = useState('Select Pricepoint');
 
+
+    useEffect(() => {
+        setEvents(eventsData);
+    }, []);
+
+    const handleEventClick = (info) => {
+        const eventLink = info.event.extendedProps.link;
+        // Navigate to the event details page
+        window.open(eventLink, '_blank');
+    };
+    
     const genres = [
         { label: 'All Genres', value: 'all' },
-        { label: 'Plays', value: 'plays' },
-        { label: 'Musicals', value: 'musicals' },
-        { label: 'Dance', value: 'dance' },
+        { label: 'Play', value: 'Play' },
+        { label: 'Musical', value: 'Musical' },
+        { label: 'Cabaret', value: 'Cabaret' },
+        { label: 'Concert', value: 'Concert' },
+        { label: 'Dance', value: 'Dance' },
+        { label: 'Other', value: 'Other' },
     ];
 
     const sizes = [
@@ -32,64 +74,75 @@ function Calendar() {
         { label: '$$$', value: 'high' },
     ];
 
-    const handleGenreChange = (event) => {
-        setSelectedGenre(event.target.value);
+    const handleGenreChange = (selectedOption) => {
+        setSelectedGenre(selectedOption.value); 
+        setGenrePlaceholder(selectedOption ? selectedOption.label : 'Select Genre');
     };
 
-    const handleSizeChange = (event) => {
-        setSelectedSize(event.target.value);
+    const handleSizeChange = (selectedOption) => {
+        setSelectedSize(selectedOption.value); 
+        setSizePlaceholder(selectedOption ? selectedOption.label : 'Select Audience Size');
     };
 
-    const handleCostChange = (event) => {
-        setSelectedCost(event.target.value);
+    const handleCostChange = (selectedOption) => {
+        setSelectedCost(selectedOption.value); 
+        setCostPlaceholder(selectedOption ? selectedOption.label : 'Select Pricepoint');
     };
-
-    const events = [
-        { title: 'As You Like It', start: '2024-03-01', genre: 'plays', size: 'medium', cost: 'high'},
-        { title: 'Sweeney Todd', start: '2024-03-03', genre: 'musicals', size: 'large', cost: 'low'},
-        { title: 'Coppelia', start: '2024-03-07', genre: 'dance', size: 'small', cost: 'medium'}
-        // Add more events with categories
-    ];
 
     const filteredEvents = selectedGenre === 'all' && selectedSize === 'all' && selectedCost === 'all'
     ? events
     : events.filter(event => (
-        (selectedGenre === 'all' || event.genre === selectedGenre) &&
+        (selectedGenre === 'all' || event.type === selectedGenre) &&
         (selectedSize === 'all' || event.size === selectedSize) &&
         (selectedCost === 'all' || event.cost === selectedCost)
     ));
 
+    const mappedEvents = filteredEvents.map(event => ({
+        ...event,
+        title: event.eventName // Rename 'eventName' to 'title'
+    }));
+
+
     return (
-        <div style={{ padding: '20px' }}>
-            <div style={{ marginBottom: '10px' }}>
-                <select value={selectedGenre} onChange={handleGenreChange} style={{marginRight: '10px'}}>
-                    {genres.map(genre => (
-                    <option key={genre.value} value={genre.value}>{genre.label}</option>
-                    ))}
-                </select>
-                <select value={selectedSize} onChange={handleSizeChange} style={{marginRight: '10px'}}>
-                    {sizes.map(size => (
-                    <option key={size.value} value={size.value}>{size.label}</option>
-                    ))}
-                </select>
-                <select value={selectedCost} onChange={handleCostChange} style={{marginRight: '10px'}}>
-                    {costs.map(cost => (
-                    <option key={cost.value} value={cost.value}>{cost.label}</option>
-                    ))}
-                </select>
+        <div className="calPage">
+            <div className="dropMenu">
+                <Select 
+                    value={selectedGenre} 
+                    onChange={handleGenreChange} 
+                    options={genres}
+                    placeholder={genrePlaceholder}
+                    styles={customStyles}
+                />
+                <Select 
+                    value={selectedSize} 
+                    onChange={handleSizeChange} 
+                    options={sizes}
+                    placeholder={sizePlaceholder}
+                    styles={customStyles}
+                />
+                <Select 
+                    value={selectedCost} 
+                    onChange={handleCostChange} 
+                    options={costs}
+                    placeholder={costPlaceholder}
+                    styles={customStyles}
+                />
             </div> 
-          <FullCalendar
-            plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
-            initialView="dayGridMonth"
-            headerToolbar={{
-              start: '',
-              center: 'title',
-              end: 'prev,next',
-            }}
-            height="90vh"
-            events={filteredEvents}
-            eventColor='#CFB034'
-          />
+            <FullCalendar
+                plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+                initialView="dayGridMonth"
+                headerToolbar={{
+                start: '',
+                center: 'title',
+                end: 'prev,next',
+                }}
+                height="90vh"
+                events={mappedEvents}
+                eventColor='#CFB034'
+                eventBorderColor='black'
+                eventTextColor='black'
+                eventClick={handleEventClick}
+            />
         </div>
     );
 }
