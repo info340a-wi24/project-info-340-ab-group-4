7af8ee0d62a5e4eb8a208c5b216868a472f50a35
salleyfang <skfang@uw.edu>
2024-03-10 23:23:09 -0700

    fixed error pages

--- src/error.js
+++ src/error.js
@@ -3,12 +3,8 @@ import React from 'react';
 export default function ErrorPage() {
   return (
     <div className='errorPage'>
-        <div>
-            <p>404</p>
-        </div>
-        <div className='errorSmall'>
-            <p>Page Not Found</p>
-        </div>
+        <h1>404</h1>
+        <h2>Page Not Found.</h2>
     </div>
   );
 }
